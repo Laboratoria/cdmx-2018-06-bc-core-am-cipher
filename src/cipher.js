ad3a46aa58cef4f4667f993da84b2f452df56dad
GenesisMauries <exodo_54@hotmail.com>
2018-06-13 11:21:02 -0500

    Subo Cipher y avance CSS

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,48 @@
 window.cipher = {
-
-};
-
-const textOneOne = () => {
-    const enter = document.getElementById("theName").value.toUpperCase(); //Valor de entrada
-    //console.log(enter); //Prueba de que se guarda el valor
-    const number = parseInt(document.getElementById("theNumber").value); //Numero de la suerte
-    //console.log(number); //Prueba de que se guarda number
-    let ascii2 = "";
-    for(let i =0; i<enter.length; i++) { 
-        //console.log(enter[i]); //Prueba de que se imprime el valor
-        let x = enter.charCodeAt(i);//Vuelve codigo ASCII
-        //console.log(x);//Prueba que se ejecuta el charCodeAt  
-        let ascii = ((x-65 + number)%26 + 65); //Formula de transformacion
-        //console.log(ascii);
-        ascii2 += String.fromCharCode(ascii);
+//La funcion dentro del objeto se declara con dos puntos :
+encode: (offset,string) => { //Función cifrar
+    let newTextVal = newText.value; //Jalar el valor ingresado por el usuario
+    let number = parseInt(newNumber.value); 
+    let newNothing = "";   
+    for(let i = 0; i < newTextVal.length; i++){         
+        if(newTextVal[i].charCodeAt() == 32){ //Para espacio
+            newNothing += newTextVal[i];
+        }else if (newTextVal[i].charCodeAt() >= 65 && newTextVal[i].charCodeAt() <= 90){ //Para Mayúsculas
+            let conAscii =newTextVal[i].charCodeAt();
+            let formula = (conAscii - 65 + number)% 26 + 65;
+            let aString = String.fromCharCode(formula);
+            newNothing += aString;
+        }else if (newTextVal[i].charCodeAt() >= 97 && newTextVal[i].charCodeAt() <= 122){ //Para Minúsculas
+            let consAsciiMin = newTextVal[i].charCodeAt();
+            let formulaMin = (consAsciiMin -97 + number)%26 + 97;
+            let aStringMin = String.fromCharCode(formulaMin);
+            newNothing += aStringMin;
+        }        
     }
-    //console.log(ascii2);
-    const salida = document.getElementById("theTextTwo").value = ascii2; //Dando valor a input
+    total.value = newNothing;
+    return newNothing;//Si se habla de funciones es forzoso que se devuelvan en un return
+},
+decode: (offset,string) =>{//Función descifrar
+    let newTextDecode = newText.value; //Jalar el valor ingresado por el usuario
+    let numberDecode = parseInt(newNumber.value); //Número del usuario
+    let newNothing = ""; 
+    for(let l = 0; l < newTextDecode.length; l++){
+        if(newTextDecode[l].charCodeAt() == 32){//Espacio
+            newNothing += newTextDecode[l];
+        }else if(newTextDecode[l].charCodeAt() >= 65 && newTextDecode[l].charCodeAt() <= 90){ //Mayúsculas
+            let mayAscii = newTextDecode[l].charCodeAt();
+            let formulaDecode = (mayAscii - 65 - numberDecode)% 26 + 65;
+            let decodeStringMay = String.fromCharCode(formulaDecode);
+            newNothing += decodeStringMay;   
+        }else if(newTextDecode[l].charCodeAt() >= 97 && newTextDecode[l].charCodeAt() <= 122){ //Minúsculas
+            let minAscii = newTextDecode[l].charCodeAt();
+            let minFormulaDecode = (minAscii -97 - numberDecode)% 26 +97;
+            let decodeStringMin = String.fromCharCode(minFormulaDecode);
+            newNothing += decodeStringMin;
+        }
+    }
+    total.value = newNothing;
+    return newNothing; //Las funciones siempre devuelven algo en return
 }
 
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+const newText = document.getElementById("string"); //Texto del usuario
+const newNumber = document.getElementById("offset"); //Número del usuario
+const bottonEncode = document.getElementById("encode"); //id del boton cifrar
+const bottonDecode = document.getElementById("decode");// id del boton descifrar
+let total = document.getElementById("result"); //Input que devolvera el texto
+
+bottonEncode.addEventListener("click", event => {
+    let clickEncode = window.cipher.encode(
+        newNumber.value,
+        newText.value,
+        total.value
+    );
+});
+
+bottonDecode.addEventListener("click", event => {
+    let clickDecode = window.cipher.decode(
+        newNumber.value,
+        newText.value,
+        total.value
+    );
+});
 
 
 
