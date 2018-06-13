478c52d01cea9a6ee98d1585f0d32cd873fd8dbb
Dani1592 <danielagutierrez1592@gmail.com>
2018-06-13 14:43:14 -0500

    actualizacion de archivos

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,39 @@
 window.cipher = {
-  // ...
+ encode:(dato,offset) => {
+
+    let ca = [];
+    let cc = [];
+    offset=parseInt(offset.value);
+
+    dato = dato.value.split('');
+
+    for (let i = 0; i < dato.length; i++){
+      let x = dato[i].charCodeAt(0)
+      ca[i] =(x+65+ offset)%26+65
+      cc[i] = String.fromCharCode(ca[i]);
+
+    }
+    //dato2.value=ca;
+
+    dato3.value=cc.join('');
+  console.log(cc.join(''))
+},
+  //comentario sin funcion
+  decode:(dato3,offset) => {
+
+    let ca = [];
+    let cc = [];
+    offset=parseInt(offset.value);
+    dato3= dato3.value.split('');
+
+    for (let i = 0; i < dato3.length; i++){
+      let x = dato3[i].charCodeAt(0)
+      ca[i] = x
+      ca[i] =(x-65- offset)%26+65
+      cc[i] = String.fromCharCode(ca[i]);
+    }
+    console.log(ca,cc)
+    dato.value=cc.join('');
+  },
+
 };
--- src/index.js
+++ src/index.js
@@ -1,36 +1,3 @@
-function cifradoCesar() {
-  let dato= document.getElementById('caja1').value;
-  let dato2= document.getElementById('caja2');
-  let dato3= document.getElementById('caja3');
-  let ca = [];
-  let cc = [];
-  dato= dato.split('');
-
-  for (let i = 0; i < dato.length; i++){
-    let x = dato[i].charCodeAt(0)
-    ca[i] = x
-    ca[i] =(x-65+3)%26+65
-    cc[i] = String.fromCharCode(ca[i]);
-  }
-  dato2.value=ca;
-  dato3.value=cc.join('');
-}
-//comentario sin funcion
-function cifradoCesarTwo() {
-  let dato= document.getElementById('caja1');
-  let dato2= document.getElementById('caja2');
-  let dato3= document.getElementById('caja3').value;
-  let ca = [];
-  let cc = [];
-  dato3= dato3.split('');
-
-  for (let i = 0; i < dato3.length; i++){
-    let x = dato3[i].charCodeAt(0)
-    ca[i] = x
-    ca[i] =(x-65-3)%26+65
-    cc[i] = String.fromCharCode(ca[i]);
-  }
-  console.log(ca,cc)
-  dato2.value=ca;
-  dato.value=cc.join('');
-}
+let dato= document.getElementById('caja1');
+let offset= document.getElementById('caja2');
+let dato3= document.getElementById('caja3');
