887d9ad6ae11d161fc7464363a232fd2c165877e
Dani1592 <danielagutierrez1592@gmail.com>
2018-06-15 13:33:00 -0500

    ultimito

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,60 @@
 window.cipher = {
  encode:(dato,offset) => {
+    let ca = []; //esta variable es cifrado ascii
+    let cc = [];//esta variable es cifrado cesar
+    offset=parseInt(offset);
 
-    let ca = [];
-    let cc = [];
-    offset=parseInt(offset.value);
-
-    dato = dato.value.split('');
 
     for (let i = 0; i < dato.length; i++){
-      let x = dato[i].charCodeAt(0)
-      ca[i] =(x+65+ offset)%26+65
-      cc[i] = String.fromCharCode(ca[i]);
+      let letra = dato[i].charCodeAt(0)
 
+      console.log(letra)
+      if(letra >= 65 && letra <= 90 ){
+        ca[i] =(letra - 65 + offset) % 26 + 65
+        cc[i] = String.fromCharCode(ca[i]);
+      } else if (letra >= 97 && letra <= 122) {
+        ca[i] =(letra - 97 + offset) % 26 + 97
+        cc[i] = String.fromCharCode(ca[i]);
+      } else {
+        cc[i] = String.fromCharCode(letra);
+      }
     }
-    //dato2.value=ca;
 
-    dato3.value=cc.join('');
-  console.log(cc.join(''))
+    console.log(dato, cc)
+
+let resultado = cc.join('');
+    return resultado
+    console.log(cc.join(''))
 },
   //comentario sin funcion
-  decode:(dato3,offset) => {
+ decode:(dato3,offset) => {
 
     let ca = [];
     let cc = [];
-    offset=parseInt(offset.value);
-    dato3= dato3.value.split('');
+    //console.log(offset)
+    offset=parseInt(offset);
+
 
     for (let i = 0; i < dato3.length; i++){
-      let x = dato3[i].charCodeAt(0)
-      ca[i] = x
-      ca[i] =(x-65- offset)%26+65
-      cc[i] = String.fromCharCode(ca[i]);
+      let letra = dato3[i].charCodeAt(0)
+
+      //console.log(letra,offset)
+      if(letra >= 65 && letra <= 90 ){
+        ca[i] =(letra - 65 - offset) % 26 + 65
+        cc[i] = String.fromCharCode(ca[i]);
+        console.log(ca[i])
+      } else if (letra >= 97 && letra <= 122) {
+        ca[i] =(letra - 97 - offset) % 26 + 97
+        cc[i] = String.fromCharCode(ca[i]);
+      } else {
+        cc[i] = String.fromCharCode(letra);
+      }
     }
     console.log(ca,cc)
-    dato.value=cc.join('');
+
+   dato5.value = cc.join('');
+    console.log(dato5.value)
+     return dato = cc.join('');
   },
 
 };
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,16 +10,21 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33',() =>{
+
+       assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33),"HIJKLMNOPQRSTUVWXYZABCDEFG");
+  });
   });
 
+});
   describe('cipher.decode', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33',() =>{
+      assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33),"HIJKLMNOPQRSTUVWXYZABCDEFG");
   });
 
   describe('cipher.createCipherWithOffset', () => {
